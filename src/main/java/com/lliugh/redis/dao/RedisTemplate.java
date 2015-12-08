8a01f17f6c724c9dd39e2b16f781505fe145f1b1
lliugh <guohaoliu@gmail.com>
2015-12-08 16:34:58 +0800

    first commit

---
+++ src/main/java/com/lliugh/redis/dao/RedisTemplate.java
@@ -0,0 +1,9 @@
+package com.lliugh.redis.dao;
+
+/**
+ * @author lliugh & guohaoliu@gmail.com & http://github.com/lliugh
+ * @(#)RedisTemplate.java 1.0 08/12/2015
+ */
+
+public class RedisTemplate {
+}
